f6a09d99c43fe34e5aa5d1703fb728fb2bd2b1c3
ddou <yutaodou@gmail.com>
2018-04-16 16:09:00 +0800

    Switch to HTTPS

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
   // and load the index.html of the app.
-  mainWindow.loadURL('http://devdocs.io');
+  mainWindow.loadURL('https://devdocs.io');
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools();
